95cf7d504fc54b93a785ca24b21ea22313e2dd65
jpmarindiaz <jpmarindiaz@gmail.com>
2016-12-07 11:34:13 -0500

    sampleData options fixes #3

--- tests/testthat/test_fringe.R
+++ tests/testthat/test_fringe.R
@@ -89,7 +89,6 @@ test_that("Sample Data", {
   t <- sampleData("Ca-Nu", asFringe = TRUE)
   expect_equal(getFtype(t),"Ca-Nu")
   expect_error(sampleData("XXXXXX", asFringe = TRUE))
-
   t <- sampleData("Ca-Nu", gt0 = FALSE)
   expect_true(any(t$b < 0))
 })
